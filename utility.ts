fc9dff77031c5d129d09217e703beb88c04dbcd3
ghubliming <139686052+ghubliming@users.noreply.github.com>
2025-03-16 23:28:47 +0100

    main code created, need to compile and fix other codes

--- utility.ts
+++ utility.ts
@@ -1,75 +1,29 @@
-export function convertPathToHtmlFragment(
-  path: string,
-  pathContainsFileName: boolean,
-  includeIcons: boolean
-): DocumentFragment {
-  const fragment = new DocumentFragment();
-  const pathParts = getPathParts(path, pathContainsFileName);
-  pathParts.forEach((part) => {
-    fragment.appendChild(getNodeForPathPart(part, includeIcons));
-  });
-  return fragment;
-}
+import { Plugin } from 'obsidian';
 
-export function getNodeForPathPart(
-  part: PathPart,
-  includeIcon: boolean
-): DocumentFragment {
-  const fragment = new DocumentFragment();
-  switch (part.type) {
-    case PATH_PART_TYPE.FOLDER:
-      if (includeIcon) {
-        fragment.appendText(`📁 `);
+export default class PathCopierPlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: 'copy-current-path',
+      name: 'Copy Current Path',
+      callback: async () => {
+        const af = this.app.workspace.getActiveFile();
+        if (!af) return;
+        await navigator.clipboard.writeText(af.path);
       }
-      fragment.appendText(part.name);
-      break;
-
-    case PATH_PART_TYPE.FILE:
-      if (includeIcon) {
-        fragment.appendText(`📄 `);
-      }
-      fragment.appendText(part.name);
-      break;
-
-    case PATH_PART_TYPE.SEPARATOR:
-      fragment.appendText(` » `);
-      break;
-  }
-  return fragment;
-}
-
-export function getPathParts(
-  path: string,
-  lastPartIsFile: boolean
-): PathPart[] {
-  if (path === '/') {
-    return [{ type: PATH_PART_TYPE.FOLDER, name: 'Vault Root' }];
-  } else {
-    const parts = path.split('/');
-    const pathParts: PathPart[] = [];
-    parts.forEach((partName) => {
-      pathParts.push({ type: PATH_PART_TYPE.FOLDER, name: partName });
-      pathParts.push({ type: PATH_PART_TYPE.SEPARATOR });
     });
 
-    // Remove last separator
-    pathParts.pop();
-
-    if (lastPartIsFile) {
-      pathParts[pathParts.length - 1].type = PATH_PART_TYPE.FILE;
+    this.registerEvent(
+      this.app.workspace.on('file-menu', (menu, file) => {
+        menu.addItem((item) => {
+          item
+            .setTitle('Copy Current Path')
+            .setIcon('copy')
+            .onClick(async () => {
+              if (!file) return;
+              await navigator.clipboard.writeText(file.path);
+            });
+        });
+      })
+    );
     }
-
-    return pathParts;
   }
-}
-
-export interface PathPart {
-  type: PATH_PART_TYPE;
-  name?: string;
-}
-
-export const enum PATH_PART_TYPE {
-  FOLDER,
-  FILE,
-  SEPARATOR,
-}
